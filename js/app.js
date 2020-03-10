dddb85cb4a23aef64b48915047c25e434e4ed0f6
francescodt <francescodtroiano@gmail.com>
2020-03-10 13:22:32 -0500

    got chart to work

--- js/app.js
+++ js/app.js
@@ -23,28 +23,42 @@ function busSkyMallProduct(name, imageUrl) {
 
 }
 
-//Create the skymall objects with the 'new' operator
 
-new busSkyMallProduct('bag', '../images/bag.jpg');
-new busSkyMallProduct('banana', '../images/banana.jpg');
-new busSkyMallProduct('bathroom', '../images/bathroom.jpg');
-new busSkyMallProduct('boots', '../images/boots.jpg');
-new busSkyMallProduct('breakfast', '../images/breakfast.jpg');
-new busSkyMallProduct('bubblegum', '../images/bubblegum.jpg');
-new busSkyMallProduct('chair', '../images/chair.jpg');
-new busSkyMallProduct('cthulhu', '../images/cthulhu.jpg');
-new busSkyMallProduct('dog-duck', '../images/dog-duck.jpg');
-new busSkyMallProduct('dragon', '../images/dragon.jpg');
-new busSkyMallProduct('pen', '../images/pen.jpg');
-new busSkyMallProduct('pet-sweep', '../images/pet-sweep.jpg');
-new busSkyMallProduct('scissors', '../images/scissors.jpg');
-new busSkyMallProduct('shark', '../images/shark.jpg');
-new busSkyMallProduct('sweep', '../images/sweep.png');
-new busSkyMallProduct('tauntaun', '../images/tauntaun.jpg');
-new busSkyMallProduct('unicorn', '../images/unicorn.jpg');
-new busSkyMallProduct('usb', '../images/usb.gif');
-new busSkyMallProduct('water-can', '../images/water-can.jpg');
-new busSkyMallProduct('wine-glass', '../images/wine-glass.jpg');
+function getBusProductArray(nameOfThePropertyIWant) {
+    var answer = [];
+    for(var i = 0; i < allBusMallPictures.length; i++){
+      answer[i] = allBusMallPictures[i][nameOfThePropertyIWant];
+    }
+    console.log(answer);
+    return answer;
+}
+  
+  busSkyMallProduct.prototype.toPercentage = function(){
+
+};
+
+
+//Create the skymall objects with the 'new' operator
+new busSkyMallProduct('R2-D2 suitcase', '../images/bag.jpg');
+new busSkyMallProduct('Banana Slicer', '../images/banana.jpg');
+new busSkyMallProduct('Pee Pad', '../images/bathroom.jpg');
+new busSkyMallProduct('Hipster Rainboots', '../images/boots.jpg');
+new busSkyMallProduct('All-in-one breakfast machine', '../images/breakfast.jpg');
+new busSkyMallProduct('Italian Bubblegum', '../images/bubblegum.jpg');
+new busSkyMallProduct('Back-problem chair', '../images/chair.jpg');
+new busSkyMallProduct('Cthulhu', '../images/cthulhu.jpg');
+new busSkyMallProduct('Dog muzzle', '../images/dog-duck.jpg');
+new busSkyMallProduct('Rare canned GMO free Dragon Meat', '../images/dragon.jpg');
+new busSkyMallProduct('Spen', '../images/pen.jpg');
+new busSkyMallProduct('Pet Sweep', '../images/pet-sweep.jpg');
+new busSkyMallProduct('Pizza scissors', '../images/scissors.jpg');
+new busSkyMallProduct('Shark sleeping bag', '../images/shark.jpg');
+new busSkyMallProduct('Baby sweep', '../images/sweep.png');
+new busSkyMallProduct('Tauntaun sleeping bag', '../images/tauntaun.jpg');
+new busSkyMallProduct('Canned unicorn bits', '../images/unicorn.jpg');
+new busSkyMallProduct('Tentacle usb', '../images/usb.gif');
+new busSkyMallProduct('Can-waterer', '../images/water-can.jpg');
+new busSkyMallProduct('Wine prison', '../images/wine-glass.jpg');
 
 
 // busSkyMallProduct[0].timesClicked = 1
@@ -104,32 +118,86 @@ function imageWasClicked(event) {
         for (var i = 0; i < allBusMallPictures.length; i++) {
             productData(i);
         }
-        for (var j = 0; j < imageElements.length; j++) 
-        imageElements[j].removeEventListener('click', imageWasClicked);
+        for (var j = 0; j < imageElements.length; j++)
+            imageElements[j].removeEventListener('click', imageWasClicked);
+        runMyChartsNow();
     }
 
 
-    function productData(index){
+    function productData(index) {
         var asideUl = document.getElementById('voteList');
-      var pictureVoteClickData = document.createElement('li');
-      pictureVoteClickData.textContent = allBusMallPictures[index].name + ' was viewed ' + allBusMallPictures[index].timesViewedOnPage + ' times, and received ' + allBusMallPictures[index].timesClicked + ' votes, which represents ' + ((allBusMallPictures[index].timesClicked/totalClicks)* 100) + '% of total votes.';
-      asideUl.appendChild(pictureVoteClickData);
+        var pictureVoteClickData = document.createElement('li');
+        pictureVoteClickData.textContent = allBusMallPictures[index].name + ' was viewed ' + allBusMallPictures[index].timesViewedOnPage + ' times, and received ' + allBusMallPictures[index].timesClicked + ' votes, which represents ' + ((allBusMallPictures[index].timesClicked / totalClicks) * 100) + '% of total votes.';
+        asideUl.appendChild(pictureVoteClickData);
 
 
 
     }
-    
+
 
 
 
 
 }
 
-    // loop for event listener on image clickage
-    var imageElements = document.getElementsByTagName('img');
+function runMyChartsNow() {
+    // chart code thanks to https://www.chartjs.org/docs/latest/
+    var ctx = document.getElementById('resultsChart').getContext('2d');
+    // eslint-disable-next-line no-undef
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            // what does labels do?
+            labels: getBusProductArray('name'),
+            // what does datasets do?
+            // it's an array of objects
+            datasets: [{
+                // what does this label do?
+                // key, legend
+                label: '# of Votes',
+                // what does this data do?
+                // actually the values in the chart
+                data: getBusProductArray('timesClicked'),
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true,
+                        stepSize: 1
+                    }
+                }]
+            }
+        }
+    });
+}
 
-    for (var c = 0; c < imageElements.length; c++) {
-        console.log('this is the event listener for the click');
-        imageElements[c].addEventListener('click', imageWasClicked);
 
-    }
+
+
+// loop for event listener on image clickage
+var imageElements = document.getElementsByTagName('img');
+
+for (var c = 0; c < imageElements.length; c++) {
+    console.log('this is the event listener for the click');
+    imageElements[c].addEventListener('click', imageWasClicked);
+
+}
